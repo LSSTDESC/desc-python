96d885d362da291032141dc5404174b4f87dfc9b
Heather Kelly <heather@slac.stanford.edu>
2018-11-09 08:34:32 -0800

    initial commit of kernel files at NERSC

---
+++ desc-python/python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+INST_DIR=/global/common/software/lsst/common/miniconda
+source $INST_DIR/setup_current_python.sh
+
+exec python -m ipykernel $@
---
+++ setup/user_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for GROUP in `groups`
+do
+    if [[ $GROUP = "lsst" ]]; then HAS_LSST=1; fi
+done
+
+if [[ $HAS_LSST != 1 ]]; then
+    echo "Contact Richard Dubois <richard@slac.stanford.edu> or Debbie Bard <djbard@lbl.gov> to request joining the 'lsst' group."
+    exit 1
+fi
+
+module unload python
+INST_DIR=/global/common/software/lsst/common/miniconda
+source $INST_DIR/setup_dev_python.sh
+#source $INST_DIR/setup_current_python.sh
+jupyter kernelspec install $INST_DIR/kernels/desc-python --user
+jupyter kernelspec install $INST_DIR/kernels/desc-python-dev --user
+jupyter kernelspec install $INST_DIR/kernels/desc-stack --user
+jupyter kernelspec install $INST_DIR/kernels/desc-stack-dev --user
+jupyter kernelspec install $INST_DIR/kernels/desc-stack-old --user
+echo "Your JupyterLab URL is: "
+echo https://jupyter-dev.nersc.gov/user/$USER/lab/tree/$HOME
+
