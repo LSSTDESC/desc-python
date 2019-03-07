2c967a8d0a6f5ed7e168fb4223c14f316dfa379b
Heather Kelly <heather@slac.stanford.edu>
2019-03-07 14:29:22 -0800

    add a kernel for latest sims release

---
+++ jupyter-kernels/desc-sims/sims.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export STACKCVMFS=/cvmfs/sw.lsst.eu/linux-x86_64/lsst_sims
+export LSST_STACK_VERSION=sims_2_13_1
+
+module load pe_archive
+module unload python
+module swap PrgEnv-intel PrgEnv-gnu
+module swap gcc gcc/6.3.0
+module rm craype-network-aries
+module rm cray-libsci
+module unload craype
+export CC=gcc
+
+source $STACKCVMFS/$LSST_STACK_VERSION/loadLSST.bash
+setup lsst_sims
+
+export OMP_NUM_THREADS=1
+
+
+exec python -m ipykernel $@
