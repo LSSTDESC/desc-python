de513b53b70297581359ea55b2b4f82e24eee675
Heather Kelly <heather999kelly@gmail.com>
2020-07-20 12:36:48 -0700

    update setup_old for current old ver

--- jupyter-kernels/setup/setup_old_python.sh
+++ jupyter-kernels/setup/setup_old_python.sh
@@ -19,16 +19,11 @@ export LSST_PYTHON_VER=old
 
 module unload python
 module swap PrgEnv-intel PrgEnv-gnu
-module swap gcc gcc/6.3.0
+module swap gcc gcc/8.3.0
 module rm craype-network-aries
 module rm cray-libsci
 module unload craype
 export CC=gcc
-
-# NaMaster
-module load cfitsio/3.47
-module load cray-fftw/3.3.8.2
-
 
 unset PYTHONHOME
 unset PYTHONPATH
@@ -39,10 +34,15 @@ if [ -n "$DESCPYTHONPATH" ]; then
     echo "Including user python path: $DESCPYTHONPATH"
 fi 
 
-export PATH=$LSST_INST_DIR/$LSST_PYTHON_VER/bin:$PATH
-source activate stack
-echo Now using $LSST_INST_DIR/$LSST_PYTHON_VER
+export DESC_GCR_SITE='nersc'
 
-# NaMaster dependencies
-export LD_LIBRARY_PATH=$CFITSIO_DIR/lib:/global/common/software/lsst/common/miniconda/namaster-1.0/lib:$FFTW_DIR:$LD_LIBRARY_PATH
+source $LSST_INST_DIR/$LSST_PYTHON_VER/etc/profile.d/conda.sh
+conda activate desc
+if [ -n "$DESCUSERENV" ]; then
+   conda activate $DESCUSERENV
+fi
+OUTPUTPY="$(which python)"
+echo Now using "${OUTPUTPY}"
+#echo Now using $LSST_INST_DIR/$LSST_PYTHON_VER/envs/desc
 
+export HDF5_USE_FILE_LOCKING=FALSE
