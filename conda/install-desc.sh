8be97372552fe147cedd6cf8fa1500dc3a81818b
Heather Kelly <heather@slac.stanford.edu>
2021-01-14 17:02:15 -0500

    insert more print statements

--- conda/install-desc.sh
+++ conda/install-desc.sh
@@ -20,9 +20,11 @@ curl -LO https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh
 bash ./Miniconda3-4.7.12.1-Linux-x86_64.sh -b -p $1
 which python
 #export PATH=$1/bin:$PATH
+echo $1
 source $1/etc/profile.d/conda.sh
 #conda activate base
 which python
+which conda
 conda env create -n desc -f $2
 conda activate desc
 
