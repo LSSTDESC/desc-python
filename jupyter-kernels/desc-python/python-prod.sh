4f7465c2670de34e7caaa2f3ee2779a1eeecb9ee
Heather Kelly <heather@slac.stanford.edu>
2021-10-21 05:57:33 -0700

    updated export, prepared desc-python jupyter kernel

--- jupyter-kernels/desc-python/python-prod.sh
+++ jupyter-kernels/desc-python/python-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INST_DIR=/global/common/software/lsst/common/miniconda
-source $INST_DIR/setup_dev_python.sh ""
+source $INST_DIR/setup_current_python.sh ""
 
 if [ $# -gt 0 ] ; then
     exec python -m ipykernel $@
