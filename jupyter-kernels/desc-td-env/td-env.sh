2ea934749c7e0d4e1ce1ffcb144cbbd3581f79f0
Heather Kelly <heather@slac.stanford.edu>
2022-04-12 14:34:01 -0400

    use setup_td.sh rather than dev

--- jupyter-kernels/desc-td-env/td-env.sh
+++ jupyter-kernels/desc-td-env/td-env.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#INST_DIR=/global/common/software/lsst/cori-haswell-gcc/stack/td_env-prod/55219
-#source $INST_DIR/setup_td_env.sh ""
 INST_DIR=/global/cfs/cdirs/lsst/groups/TD
-source $INST_DIR/setup_td_dev.sh -k
+source $INST_DIR/setup_td.sh -k
 
 if [ $# -gt 0 ] ; then
     exec python -m ipykernel $@
