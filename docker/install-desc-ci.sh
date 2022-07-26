0e9d3a82abff924d10a2c78c8ae55fc8971929ec
Heather Kelly <heather@slac.stanford.edu>
2022-07-26 02:53:56 -0400

    Update install-desc-ci.sh

--- docker/install-desc-ci.sh
+++ docker/install-desc-ci.sh
@@ -15,9 +15,9 @@ fi
 
 unset PYTHONPATH
 
-curl -LO https://repo.anaconda.com/miniconda/$3
+curl -LO https://repo.anaconda.com/miniconda/$2
 
-bash ./$3 -b -p $1
+bash ./$2 -b -p $1
 which python
 source $1/etc/profile.d/conda.sh
 conda activate base
@@ -26,12 +26,12 @@ mamba install -c conda-forge -y mpich=3.3.*=external_*
 which python
 which conda
 
-if [[ -z "$2" ]]
+if [[ -z "$3" ]]
 then
-  mamba install -c conda-forge -y --file ./condalist.txt
-  pip install --no-cache-dir -r ./piplist.txt
+  mamba install -c conda-forge -y --file ../conda/condalist.txt
+  pip install --no-cache-dir -r ../conda/piplist.txt
 else
-  mamba env update -n base -f $2
+  mamba env update -n base -f $3
 fi
 
 conda clean -y -a 
