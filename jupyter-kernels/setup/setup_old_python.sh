8dcc2e40665681a29baece48d7685cc432e7bf8f
Heather Kelly <heather@slac.stanford.edu>
2022-07-25 21:32:52 -0700

    move current prod to old

--- jupyter-kernels/setup/setup_old_python.sh
+++ jupyter-kernels/setup/setup_old_python.sh
@@ -1,4 +1,49 @@
-#!/bin/bash
+#!/bin/sh
+function log () {
+    if [[ $_V -eq 1 ]]; then
+        echo "$@"
+    fi
+}
 
-shifter --image=lsstdesc/desc-python:old bash --init-file /global/common/software/lsst/common/miniconda/kernels/setup_old_python_kernel.sh
+_V=0
+while getopts "v" OPTION
+do
+  case $OPTION in
+    v) _V=1
+       ;;
+  esac
+done
+
+export LSST_INST_DIR=/global/common/software/lsst/common/miniconda
+export LSST_PYTHON_VER=old
+
+module unload python
+module swap PrgEnv-intel PrgEnv-gnu
+module rm craype-network-aries
+module rm cray-libsci
+module unload craype
+export CC=gcc
+
+unset PYTHONHOME
+unset PYTHONPATH
+export PYTHONNOUSERSITE=' '
+
+if [ -n "$DESCPYTHONPATH" ]; then
+    export PYTHONPATH="$DESCPYTHONPATH"
+    echo "Including user python path: $DESCPYTHONPATH"
+fi 
+
+export DESC_GCR_SITE='nersc'
+
+source $LSST_INST_DIR/$LSST_PYTHON_VER/etc/profile.d/conda.sh
+conda activate desc
+if [ -n "$DESCUSERENV" ]; then
+   conda activate $DESCUSERENV
+fi
+OUTPUTPY="$(which python)"
+echo Now using "${OUTPUTPY}"
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+
 
