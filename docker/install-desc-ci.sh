d2e24b2582a5496d4431f5e856c6cb7e876cbdb7
Heather Kelly <heather@slac.stanford.edu>
2022-07-26 02:47:42 -0400

    Create install-desc-ci.sh

---
+++ docker/install-desc-ci.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+## To Run a fresh install
+## bash install-desc-python.sh <pathToCondaInstall> 
+
+## To Install using an input YAML file
+## bash install-desc-python.sh <pathToCondaInstall> ./desc-python-env-nersc-install-nobuildinfo.yml
+
+if [ -z "$1" ]
+then	
+	echo "Please provide a full path install directory"
+	exit 1
+fi
+
+
+unset PYTHONPATH
+
+curl -LO https://repo.anaconda.com/miniconda/$3
+
+bash ./$3 -b -p $1
+which python
+source $1/etc/profile.d/conda.sh
+conda activate base
+conda install -c conda-forge -y mamba
+mamba install -c conda-forge -y mpich=3.3.*=external_*
+which python
+which conda
+
+if [[ -z "$2" ]]
+then
+  mamba install -c conda-forge -y --file ./condalist.txt
+  pip install --no-cache-dir -r ./piplist.txt
+else
+  mamba env update -n base -f $2
+fi
+
+conda clean -y -a 
+#python -m compileall $1
+
+# Install jupyterlab at CC
+if [[ -z "$NERSC_HOST" ]]
+then	
+  pip install jupyterlab
+fi
+
+conda env export --no-builds > $1/desc-python-nobuildinfo.yml
+conda env export > $1/desc-python.yml
+
+
+
