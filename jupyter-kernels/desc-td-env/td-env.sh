833324be283b06e44693ddd57c0c90a86579bb6e
Heather Kelly <heather@slac.stanford.edu>
2023-03-27 10:24:50 -0400

    Set up cosmosis

--- jupyter-kernels/desc-td-env/td-env.sh
+++ jupyter-kernels/desc-td-env/td-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INST_DIR=/global/cfs/cdirs/lsst/groups/TD
-source $INST_DIR/setup_td.sh -k
+source $INST_DIR/setup_td.sh -k -c
 
 if [ $# -gt 0 ] ; then
     exec python -m ipykernel $@
