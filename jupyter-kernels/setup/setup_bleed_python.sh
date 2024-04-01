704babba88184d9348c6211b53ebe07913d67c39
Heather Kelly <heather@slac.stanford.edu>
2024-04-01 09:59:07 -0700

    add support for PYTHONUSERBASE setup using DESCPYTHONUSERBASE

--- jupyter-kernels/setup/setup_bleed_python.sh
+++ jupyter-kernels/setup/setup_bleed_python.sh
@@ -20,6 +20,20 @@ if [ -n "$DESCUSERENV" ]; then
    conda activate $DESCUSERENV
 fi
 
+python_ver_major=$(python -c 'import sys; print(sys.version_info.major)')
+python_ver_minor=$(python -c 'import sys; print(sys.version_info.minor)')
+export DESCPYTHONVER="python$python_ver_major.$python_ver_minor"
+
+export PYTHONPATH=$PYTHONPATH:$LSST_INST_DIR/$LSST_PYTHON_VER
+
+if [ -n "$DESCPYTHONBLEEDUSERBASE" ]; then
+    export PYTHONUSERBASE=$DESCPYTHONBLEEDUSERBASE
+    unset PYTHONUSERSITE
+    export PYTHONPATH="$PYTHONUSERBASE/lib/$DESCPYTHONVER/site-packages:$PYTHONPATH"
+    echo "using DESCPYTHONBLEEDUSERBASE: $DESCPYTHONBLEEDUSERBASE"
+fi
+
+
 export HDF5_USE_FILE_LOCKING=FALSE
 
 if [ $# -gt 0 ] ; then
