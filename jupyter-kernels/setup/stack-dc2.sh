39da76095fdabdbffe8e78b64090dae81c7a9c3e
Heather Kelly <heather@slac.stanford.edu>
2026-04-05 16:06:31 -0700

    update stack setup to move on from DC2

--- jupyter-kernels/setup/stack-dc2.sh
+++ jupyter-kernels/setup/stack-dc2.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 source /opt/lsst/software/stack/loadLSST.bash ""
 setup lsst_distrib
 [ -d "$LSST_HOME/obs_lsst" ] && setup -r $LSST_HOME/obs_lsst
+setup lsst_sims
 [ -d "$LSST_HOME/supreme" ] && setup -r $LSST_HOME/supreme -j
-[ -d "/opt/lsst/software/stack/meas_pz" ] && setup -r /opt/lsst/software/stack/meas_pz -j
-
 export OMP_NUM_THREADS=1
 
 unset PYTHONSTARTUP
@@ -13,8 +12,6 @@ unset PYTHONSTARTUP
 export PYTHONNOUSERSITE=' '
 
 export DESC_GCR_SITE='nersc'
-
-export RUBIN_SIM_DATA_DIR=/global/cfs/cdirs/lsst/groups/CO/rubin_sim
 
 if [ -n "$DESCPYTHONPATH" ]; then
     export PYTHONPATH="$DESCPYTHONPATH:$PYTHONPATH"
@@ -26,20 +23,6 @@ if [ -n "$DESCSTACKUSERENV" ]; then
    conda activate $DESCSTACKUSERENV
    echo "Activated your DESCSTACKUSERENV: $DESCSTACKUSERENV"
     echo "Wondering Why? DESCSTACKUSERENV is likely set in your $HOME/.basrhc, $HOME/.bashrc.ext, or similar config script"
-fi
-
-python_ver_major=$(python -c 'import sys; print(sys.version_info.major)')
-python_ver_minor=$(python -c 'import sys; print(sys.version_info.minor)')
-export DESCPYTHONVER="python$python_ver_major.$python_ver_minor"
-
-export PYTHONPATH=$PYTHONPATH:$LSST_INST_DIR/$LSST_PYTHON_VER
-
-if [ -n "$DESCSTACKUSERBASE" ]; then
-    export PYTHONUSERBASE=$DESCSTACKUSERBASE
-    unset PYTHONUSERSITE
-    export PATH=$PYTHONUSERBASE/bin:$PATH       
-    export PYTHONPATH="$PYTHONUSERBASE/lib/$DESCPYTHONVER/site-packages:$PYTHONPATH"
-    echo "using DESCSTACKUSERBASE: $DESCSTACKUSERBASE"
 fi
 
 
--- jupyter-kernels/setup/stack-weekly.sh
+++ jupyter-kernels/setup/stack-weekly.sh
@@ -4,7 +4,6 @@ source /opt/lsst/software/stack/loadLSST.bash ""
 setup lsst_distrib
 [ -d "$LSST_HOME/obs_lsst" ] && setup -r $LSST_HOME/obs_lsst
 [ -d "$LSST_HOME/supreme" ] && setup -r $LSST_HOME/supreme -j
-[ -d "/opt/lsst/software/stack/meas_pz" ] && setup -r /opt/lsst/software/stack/meas_pz -j
 
 export OMP_NUM_THREADS=1
 
