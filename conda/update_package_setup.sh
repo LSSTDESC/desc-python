adb14a175a9537909fdf5a24ce02e1b85102663d
Heather Kelly <heather@slac.stanford.edu>
2026-05-11 10:40:18 -0700

    Create update_package_setup.sh for user installs
    Add a script for setting up user package installation.

---
+++ conda/update_package_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Allows --user install
+unset PYTHONNOUSERSITE
+
+# Point to the area where --user packages should be installed
+export PYTHONUSERBASE=$DESC_PY_EXTRA_PACKAGES
+
+echo "Ready to pip install packages"
+echo "pip install --user --no-deps --no-build-isolation <packageName>"
+echo "If updating a package include the -U option"
